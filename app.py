015507e479fc7c130d32b1fd756d80041f9dc3f9
Saketh Mallojjala <110251683+saketh-mallojjala@users.noreply.github.com>
2023-10-21 13:45:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,10 @@ st.write("Upload a PDF file for text extraction:")
 uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
 if uploaded_file is not None:
     if st.button("Extract Text"):
-        text = extract_text_from_pdf(str(uploaded_file))
+         file_contents = uploaded_file.read()
+    # Convert bytes to a string
+        #file_contents_str = file_contents.decode('utf-8')
+        text = extract_text_from_pdf(file_contents)
 
         st.subheader("Extracted Text:")
         st.write(text)

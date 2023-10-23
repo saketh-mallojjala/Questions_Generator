9da7ecf973a3ce75e2aa9cee3e26768fc1839089
Saketh Mallojjala <110251683+saketh-mallojjala@users.noreply.github.com>
2023-10-23 20:01:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,83 +32,83 @@ if uploaded_file is not None:
                 help="Click to download the extracted text as a text file."
             )
 
-        generate_button = st.button("Generate Boolean Questions")
-        text1 = ''
-        if generate_button:
-            text = ' '.join(text.split())
-            payload = {
-                "input_text": text
-            }
-            qe = main.BoolQGen()
-            output1 = qe.predict_boolq(payload)
+        # generate_button = st.button("Generate Boolean Questions")
+        # text1 = ''
+        # if generate_button:
+        #     text = ' '.join(text.split())
+        #     payload = {
+        #         "input_text": text
+        #     }
+        #     qe = main.BoolQGen()
+        #     output1 = qe.predict_boolq(payload)
 
-            text1 = str(output1['Boolean Questions'])
-            st.write(text1)
+        #     text1 = str(output1['Boolean Questions'])
+        #     st.write(text1)
 
-        download_button1 = st.button("Download Boolean Questions File")
+        # download_button1 = st.button("Download Boolean Questions File")
 
-        if download_button1:
-            text_io = io.StringIO(text1)
-            st.write("### Download the Boolean Questions as a text file")
-            st.download_button(
-                label="Click to Download",
-                data=text_io,
-                # key="Boolean_Questions_file.txt",
-                on_click=None,
-                args=None,
-                help="Click to download the Boolean_Questions as a text file."
-            )
+        # if download_button1:
+        #     text_io = io.StringIO(text1)
+        #     st.write("### Download the Boolean Questions as a text file")
+        #     st.download_button(
+        #         label="Click to Download",
+        #         data=text_io,
+        #         # key="Boolean_Questions_file.txt",
+        #         on_click=None,
+        #         args=None,
+        #         help="Click to download the Boolean_Questions as a text file."
+        #     )
 
-        generate_button = st.button("Generate MCQ Questions")
-        text2 = ''
-        if generate_button:
-            text = ' '.join(text.split())
-            payload = {
-                "input_text": text
-            }
-            qg = main.QGen()
-            output2 = qg.predict_mcq(payload)
+        # generate_button = st.button("Generate MCQ Questions")
+        # text2 = ''
+        # if generate_button:
+        #     text = ' '.join(text.split())
+        #     payload = {
+        #         "input_text": text
+        #     }
+        #     qg = main.QGen()
+        #     output2 = qg.predict_mcq(payload)
 
-            text2 = output2['questions']
-            st.write(text2)
+        #     text2 = output2['questions']
+        #     st.write(text2)
 
-        download_button1 = st.button("Download MCQ Questions File")
+        # download_button1 = st.button("Download MCQ Questions File")
 
-        if download_button1:
-            text_io = io.StringIO(text2)
-            st.write("### Download the MCQ Questions as a text file")
-            st.download_button(
-                label="Click to Download",
-                data=text_io,
-                key="MCQ_Questions_file.txt",
-                on_click=None,
-                args=None,
-                help="Click to download the MCQ_Questions as a text file."
-            )
+        # if download_button1:
+        #     text_io = io.StringIO(text2)
+        #     st.write("### Download the MCQ Questions as a text file")
+        #     st.download_button(
+        #         label="Click to Download",
+        #         data=text_io,
+        #         key="MCQ_Questions_file.txt",
+        #         on_click=None,
+        #         args=None,
+        #         help="Click to download the MCQ_Questions as a text file."
+        #     )
 
-        generate_button = st.button("Generate One Word Answer Questions")
-        text3 = ''
-        if generate_button:
-            text = ' '.join(text.split())
-            payload = {
-                "input_text": text
-            }
-            qg = main.QGen()
-            output3 = qg.predict_shortq(payload)
+        # generate_button = st.button("Generate One Word Answer Questions")
+        # text3 = ''
+        # if generate_button:
+        #     text = ' '.join(text.split())
+        #     payload = {
+        #         "input_text": text
+        #     }
+        #     qg = main.QGen()
+        #     output3 = qg.predict_shortq(payload)
 
-            text3 = output3['questions']
-            st.write(text3)
+        #     text3 = output3['questions']
+        #     st.write(text3)
 
-        download_button1 = st.button("Download Ome Word Answer Questions File")
+        # download_button1 = st.button("Download Ome Word Answer Questions File")
 
-        if download_button1:
-            text_io = io.StringIO(text3)
-            st.write("### Download the One Word Answer Questions as a text file")
-            st.download_button(
-                label="Click to Download",
-                data=text_io,
-                key="One_Word_Answer_Questions_file.txt",
-                on_click=None,
-                args=None,
-                help="Click to download the One_Word_Answer_Questions as a text file."
-            )
+        # if download_button1:
+        #     text_io = io.StringIO(text3)
+        #     st.write("### Download the One Word Answer Questions as a text file")
+        #     st.download_button(
+        #         label="Click to Download",
+        #         data=text_io,
+        #         key="One_Word_Answer_Questions_file.txt",
+        #         on_click=None,
+        #         args=None,
+        #         help="Click to download the One_Word_Answer_Questions as a text file."
+        #     )
